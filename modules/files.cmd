0b27014ad3a2794fb94866c9e09003409d8911b6
sebthom <sebthom@users.noreply.github.com>
2023-03-30 00:54:25 +0200

    add basename

--- modules/files.cmd
+++ modules/files.cmd
@@ -7,7 +7,29 @@ call :%* ""
 goto :eof
 
 
-:read_first_line_of_file <FILE_PATH> <RESULT_VAR>
+:basename <PATH> [<RESULT_VAR>]
+  setlocal
+  set basename=%~nx1
+  set result_var=%~2
+  if "%result_var%" == "" (
+    echo %basename%
+    exit /B 0
+  )
+  endlocal & set "%result_var%=%basename%"
+
+
+:basename_without_extension <PATH> <RESULT_VAR>
+  setlocal
+  set basename=%~n1
+  set result_var=%~2
+  if "%result_var%" == "" (
+    echo %basename%
+    exit /B 0
+  )
+  endlocal & set "%result_var%=%basename%"
+
+
+:read_first_line_of_file <FILE_PATH> [<RESULT_VAR>]
   setlocal
   set file_path=%~1
   set result_var=%~2
